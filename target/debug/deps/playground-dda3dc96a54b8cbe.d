17c0adee6e5b46b543e12f827d5b6842e4b419e5
Flynn500 <flynnwilson101@gmail.com>
2026-01-11 11:52:11 +1300

    aa

---
+++ target/debug/deps/playground-dda3dc96a54b8cbe.d
@@ -0,0 +1,5 @@
+C:\Users\Flynn Wilson\Documents\RustProjects\substratum\target\debug\deps\libplayground-dda3dc96a54b8cbe.rmeta: src\bin\playground.rs
+
+C:\Users\Flynn Wilson\Documents\RustProjects\substratum\target\debug\deps\playground-dda3dc96a54b8cbe.d: src\bin\playground.rs
+
+src\bin\playground.rs:
---
+++ target/debug/deps/substratum-f6bec6a8635fe93c.d
@@ -0,0 +1,17 @@
+C:\Users\Flynn Wilson\Documents\RustProjects\substratum\target\debug\deps\libsubstratum-f6bec6a8635fe93c.rmeta: src\lib.rs src\array\mod.rs src\array\broadcast.rs src\array\ndarray.rs src\array\shape.rs src\array\storage.rs src\ops\mod.rs src\ops\arithmetic.rs src\ops\scalar.rs src\ops\unary.rs src\random\mod.rs src\random\seed.rs src\random\generator.rs
+
+C:\Users\Flynn Wilson\Documents\RustProjects\substratum\target\debug\deps\substratum-f6bec6a8635fe93c.d: src\lib.rs src\array\mod.rs src\array\broadcast.rs src\array\ndarray.rs src\array\shape.rs src\array\storage.rs src\ops\mod.rs src\ops\arithmetic.rs src\ops\scalar.rs src\ops\unary.rs src\random\mod.rs src\random\seed.rs src\random\generator.rs
+
+src\lib.rs:
+src\array\mod.rs:
+src\array\broadcast.rs:
+src\array\ndarray.rs:
+src\array\shape.rs:
+src\array\storage.rs:
+src\ops\mod.rs:
+src\ops\arithmetic.rs:
+src\ops\scalar.rs:
+src\ops\unary.rs:
+src\random\mod.rs:
+src\random\seed.rs:
+src\random\generator.rs:
