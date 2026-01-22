eb89b5f5b43fcd1d7e240caaf28efda8df961b84
Flynn500 <flynnwilson101@gmail.com>
2026-01-22 20:51:27 +1300

    Added results for all trees

--- src/python/tree_benchmark.py
+++ src/python/tree_benchmark.py
@@ -2,7 +2,7 @@ import time
 import substratum as ss
 
 
-def benchmark_balltree(
+def benchmark_tree(
     n_points=100_000,
     n_queries=1_000,
     dim=2,
@@ -21,7 +21,7 @@ def benchmark_balltree(
 
     for _ in range(runs):
         t0 = time.perf_counter()
-        tree = ss.spatial.BallTree.from_array(
+        tree = ss.spatial.VPTree.from_array(
             points,
             leaf_size=leaf_size,
             metric="euclidean",
@@ -46,7 +46,7 @@ def benchmark_balltree(
     }
 
 if __name__ == "__main__":
-    result = benchmark_balltree(
+    result = benchmark_tree(
         n_points=200_000,
         n_queries=1000,
         dim=2,
@@ -54,25 +54,54 @@ if __name__ == "__main__":
         runs=10,
     )
 
-    print("BallTree benchmark results:")
+    print("VPTree benchmark results:")
     for k, v in result.items():
         print(f"{k:>12}: {v:.6f} sec")
 
 """
+KDE Query - Gaussian, Euclidian
+n_points=200_000,
+n_queries=1000,
+
+
 old cache hating implementation:
 
+KDTree benchmark results:
+    build_min: 0.035546 sec
+    build_mean: 0.037652 sec
+    query_min: 3.458297 sec
+    query_mean: 3.813541 sec
+
 BallTree benchmark results:
-   build_min: 0.050904 sec
-  build_mean: 0.053747 sec
-   query_min: 3.484985 sec
-  query_mean: 3.655839 sec
+    build_min: 0.050904 sec
+    build_mean: 0.053747 sec
+    query_min: 3.484985 sec
+    query_mean: 3.655839 sec
+
+VPTree benchmark results:
+    build_min: 0.035535 sec
+    build_mean: 0.037596 sec
+    query_min: 3.428210 sec
+    query_mean: 3.515841 sec
 
 
 new cache friendly implementation:
 
-   build_min: 0.043306 sec
-  build_mean: 0.046842 sec
-   query_min: 2.473859 sec
-  query_mean: 2.506054 sec
+KDTree benchmark results:
+    build_min: 0.033573 sec
+    build_mean: 0.035239 sec
+    query_min: 2.342033 sec
+    query_mean: 2.391603 sec
 
+BallTree benchmark results:
+    build_min: 0.043306 sec
+    build_mean: 0.046842 sec
+    query_min: 2.473859 sec
+    query_mean: 2.506054 sec
+
+VPTree benchmark results:
+    build_min: 0.034412 sec
+    build_mean: 0.035934 sec
+    query_min: 2.354319 sec
+    query_mean: 2.403241 sec
 """
